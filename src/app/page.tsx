4e08812dbc0804efde0c3799bae12d60e4b80e3d
slmyer <slmyer@github.com>
2024-12-29 21:39:13 +0800

    feat: init home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 sm:p-20">
-      123432 2
+    <div className="">
+      <main>
+        <section>Slmyer Blog</section>
+        <section>{/* <h1>最新文章</h1> */}</section>
+      </main>
     </div>
   )
 }
